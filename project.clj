9da9d37db57c18558005d7806b61bc39047401f4
oakes <zach@sekao.net>
2019-07-23 17:09:16 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject play-cljc.text "0.2.0-SNAPSHOT"
+(defproject play-cljc.text "0.2.0"
   :description "A library for rendering text with play-cljc"
   :url "https://github.com/oakes/play-cljc.text"
   :license {:name "Public Domain"
